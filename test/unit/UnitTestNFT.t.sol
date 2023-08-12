c7761d53b35139e6456cca6c819f310b95c28734
ScipioneParmigiano <pcs.parmigiano@gmail.com>
2023-08-12 22:35:45 +0200

    minor changes and new logo

--- test/unit/UnitTestNFT.t.sol
+++ test/unit/UnitTestNFT.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import {Test, console} from "lib/forge-std/src/Test.sol";
 import {StdCheats} from "lib/forge-std/src/StdCheats.sol";
